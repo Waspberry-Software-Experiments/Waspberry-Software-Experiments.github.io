257e7a54f2ddefc8e968a587a46d431cb491fa7f
waxmos <waxmos@hotmail.com>
2026-05-08 12:46:29 +0100

    Create wbif-2.js

---
+++ wbif-2.js
@@ -0,0 +1,102 @@
+class WBIF {
+    async fetchBuffer(url) {
+        const res = await fetch(url);
+        return await res.arrayBuffer();
+    }
+
+    parse(buffer) {
+        const view = new DataView(buffer);
+
+        const magic =
+            String.fromCharCode(
+                ...new Uint8Array(buffer, 0, 4)
+            );
+
+        if (magic !== "WBIF") {
+            throw new Error("Invalid WBIF file");
+        }
+
+        const version = view.getUint32(4, true);
+
+        let offset = 8;
+
+        const result = {
+            version,
+            width: 0,
+            height: 0,
+            metadata: {},
+            pixels: null
+        };
+
+        while (offset < buffer.byteLength) {
+            const type =
+                String.fromCharCode(
+                    ...new Uint8Array(buffer, offset, 4)
+                );
+
+            offset += 4;
+
+            const size = view.getUint32(offset, true);
+            offset += 4;
+
+            const chunk = buffer.slice(offset, offset + size);
+
+            offset += size;
+
+            if (type === "HEAD") {
+                const headView = new DataView(chunk);
+
+                result.width =
+                    headView.getUint32(0, true);
+
+                result.height =
+                    headView.getUint32(4, true);
+            }
+
+            else if (type === "META") {
+                const text =
+                    new TextDecoder().decode(chunk);
+
+                result.metadata = JSON.parse(text);
+            }
+
+            else if (type === "DATA") {
+                result.pixels =
+                    new Uint8ClampedArray(chunk);
+            }
+
+            else if (type === "END!") {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    async open(url) {
+        const buffer = await this.fetchBuffer(url);
+        return this.parse(buffer);
+    }
+
+    async getMessage(url) {
+        const img = await this.open(url);
+        return img.metadata.message;
+    }
+
+    async render(url, canvas) {
+        const img = await this.open(url);
+
+
+        const ctx = canvas.getContext("2d");
+
+        const imageData = new ImageData(
+            img.pixels,
+            img.width,
+            img.height
+        );
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+}
+
+window.wbif = new WBIF();
